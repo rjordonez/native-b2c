505860b6c16a84147cbb52f8ca1a8ff77aebbe78
rjordonez <rexjordonez@gmail.com>
2025-07-23 00:42:18 -0700

    removed redundant files

--- src/features/chat/components/index.ts
+++ src/features/chat/components/index.ts
@@ -1,7 +1,4 @@
-export { default as ConversationsSidebar } from './ConversationsSidebar';
-export { default as ConversationItem } from './ConversationItem';
 export { default as ChatMessages } from './ChatMessages';
-export { default as MessageInput } from './MessageInput';
 export { default as EmptyState } from './EmptyState';
 export { default as VoiceRecorder } from './VoiceRecorder';
 export { default as VoiceMessage } from './VoiceMessage';
