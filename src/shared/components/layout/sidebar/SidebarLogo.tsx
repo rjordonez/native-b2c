c03fa1e9ca06c7b38b2f4d094d48842728ac9f5e
rjordonez <rexjordonez@gmail.com>
2025-07-23 19:00:09 -0700

    new

--- src/shared/components/layout/sidebar/SidebarLogo.tsx
+++ src/shared/components/layout/sidebar/SidebarLogo.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import SplitScreenIcon from '@mui/icons-material/SplitScreen';
 import { useAppDispatch, useAppSelector } from '../../../../store/hooks';
 import { 
   toggleSidebarCollapse, 
@@ -76,7 +75,20 @@ const SidebarLogo: React.FC = () => {
             hoveredItem === 'logo' ? 'opacity-100' : 'opacity-0 pointer-events-none'
           }`}
         >
-          <SplitScreenIcon sx={{ fontSize: 20, transform: 'rotate(90deg)' }} />
+          <svg 
+            width="20" 
+            height="20" 
+            viewBox="0 0 24 24" 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"
+          >
+            <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+            <line x1="9" y1="3" x2="9" y2="21"></line>
+          </svg>
         </button>
 
         {/* Tooltip */}
@@ -107,7 +119,20 @@ const SidebarLogo: React.FC = () => {
           onMouseLeave={handleMouseLeave}
           className="w-10 h-10 rounded-md flex items-center justify-center text-gray-600 hover:text-black hover:bg-gray-50 transition-colors"
         >
-          <SplitScreenIcon sx={{ fontSize: 20, transform: 'rotate(90deg)' }} />
+          <svg 
+            width="20" 
+            height="20" 
+            viewBox="0 0 24 24" 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"
+          >
+            <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+            <line x1="9" y1="3" x2="9" y2="21"></line>
+          </svg>
         </button>
         
         {/* Tooltip */}
